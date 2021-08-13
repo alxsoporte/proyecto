dfc006435d9667d569b4ee240055708126f76dac
Oscar Cangui <alxserviciotecnico@gmail.com>
2021-08-13 11:00:42 -0500

    Se comparte la carpeta misitioweb que se encontraba originalmente en htdocs de mysql

---
+++ sitioweb/productos.php
@@ -0,0 +1,44 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="img/rotulo.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">ALX SOPORTE</h4>
+        <p class="card-text">Soporte Técnico en computadoras y laptops</p>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="img/monitor.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Monitor</h4>
+        <p class="card-text">Tamaño: 19,5"</p>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="img/teclado.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Teclado Razeak</h4>
+        <p class="card-text">Teclado gaming con luces led</p>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="img/mouse.jpg" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Mouse Razeak</h4>
+        <p class="card-text">Mouse gaming con luces led</p>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+    </div>
+</div>
+</div>
+
+<?php include("template/pie.php"); ?>
